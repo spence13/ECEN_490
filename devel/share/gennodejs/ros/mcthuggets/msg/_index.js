641c162b870819283cd7e3f545fc582c2a851d75
Spencer Carter <srcarter13@gmail.com>
2017-01-14 13:31:06 -0700

    created rep

---
+++ devel/share/gennodejs/ros/mcthuggets/msg/_index.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+let BallState = require('./BallState.js');
+let RobotState = require('./RobotState.js');
+let PIDInfo = require('./PIDInfo.js');
+
+module.exports = {
+  BallState: BallState,
+  RobotState: RobotState,
+  PIDInfo: PIDInfo,
+};
